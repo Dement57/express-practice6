eb08da841154ab04f3bfac5575e6776049753bb9
Dement057 <Dement057@bitbucket.org>
2022-03-31 01:22:57 +0300

    Add logs

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ import crypto from "crypto"
 import http from "http"
 import { createReadStream } from 'fs';
 import { appSrc } from "./app.js";
-// const PORT = process.env.PORT || 4321;
 const app = appSrc(x, bodyParser, createReadStream, crypto, http);
-
-app
-    .listen(process.env.PORT || 4321);
+app.listen(process.env.PORT || 4321, () => log(process.pid));
