27986be82f277f4d033786ab4c910667f7f6eecb
Dement057 <Dement057@bitbucket.org>
2022-03-31 01:13:18 +0300

    Add all routes

--- app.js
+++ app.js
@@ -1,3 +1,57 @@
-import x from 'express';
-const app = x();
-export { app };
+export function appSrc(x, bodyParser, createReadStream, crypto, http) {
+    const app = x();
+    const headers = {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Access-Control-Allow-Origin': '',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE'
+    };
+    const setHeaders = (req, res, n) => res.set(headers) && n();
+    app
+        .use(setHeaders)
+        .get('/login/', (req, res) => {
+            res.send('dement057777')
+        })
+        .get('/code/', (req, res) => {
+            const stream = createReadStream(import.meta.url.substring(8));
+            stream.pipe(res);
+        })
+        .get('/sha1/:input/', (req, res) => {
+            const inputSHA = crypto.createHash('sha1');
+            res.send(inputSHA.update(JSON.stringify(req.params.input)).digest('hex'))
+        })
+        .get('/req/', (req, res) => {
+            const queryAddress = req.query.addr;
+            http.get(queryAddress, resp => {
+                resp.setEncoding('utf8');
+                let rawData = '';
+                resp.on('data', (chunk) => { rawData += chunk; });
+                resp.on('end', () => {
+                    try {
+                        res.send(rawData);
+                    } catch (e) {
+                        console.error(e.message);
+                    }
+                });
+            });
+        })
+        .use(bodyParser.json())
+        .post('/req/', (req, res) => {
+            const url = req.body.url;
+            http.get(url, resp => {
+                resp.setEncoding('utf8');
+                let rawData = '';
+                resp.on('data', (chunk) => { rawData += chunk; });
+                resp.on('end', () => {
+                    try {
+                        res.send(rawData);
+                    } catch (e) {
+                        console.error(e.message);
+                    }
+                });
+            });
+        })
+        .use('/', (req, res) => {
+            res.send('dement057777')
+        });
+    return app;
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-import { app } from "./app.js";
+import x from "express"
+import bodyParser from "body-parser";
+import crypto from "crypto"
+import http from "http"
+import { createReadStream } from 'fs';
+import { appSrc } from "./app.js";
+// const PORT = process.env.PORT || 4321;
+const app = appSrc(x, bodyParser, createReadStream, crypto, http);
 
 app
-    .use('/', (req,res) => res.send('Привет'))
-
-    .listen(4321);
+    .listen(process.env.PORT || 4321);
